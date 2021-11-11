9968a417f18acb5c0395a127162700fda120884d
timduran <durantimothy21@gmail.com>
2021-11-11 15:15:44 -0800

    initial commit with jas

---
+++ script.js
@@ -0,0 +1,24 @@
+// Define Variables for All Passward characters IE lowercase uppercase, special characters and numbers
+var lowercase = ['a', 'b', 'c'];
+var uppercase =['A', 'B', 'C'];
+var numbers = ['0', '1',  '2'];
+var specials = ['!', '@', '#'];
+// function asks what characters the user wants in their password
+function getPasswordCriteria() {
+
+}
+
+// Generate the password using the true and false values
+function generatePassword() {
+  var options = getPasswordCriteria()
+}
+// Random function that will pick out a random character from the arrays
+
+// function to write the password using the criteria the user specified
+function writePassword(){
+  var password = generatePassword();
+}
+
+document.getElementById('generate').addEventListener('click', () =>{
+writePassword
+})
