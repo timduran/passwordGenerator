a524eb294b735845fc9406f2c2fa8f67bd37ad8d
timduran <durantimothy21@gmail.com>
2021-11-11 15:24:45 -0800

    write ask questions function

--- script.js
+++ script.js
@@ -5,7 +5,32 @@ var numbers = ['0', '1',  '2'];
 var specials = ['!', '@', '#'];
 // function asks what characters the user wants in their password
 function getPasswordCriteria() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
 
+  if( length < 8 || length >128) {
+    alert('Password length must be at least 8 characters or less than 128 characters')
+    return null;
+  }
+
+  var containLowercase = confirm('Click OK to put lowercase characters in your password');
+
+  var containUppercase = confirm('Click OK to put uppercase characters in your password');
+
+  var containNumbers = confirm('Click OK to put numerical characters in your password');
+
+  var containSpecial = confirm('Click OK to put special characters in your password');
+
+  var passwordOptions = {
+    length: length,
+    containLowercase: containLowercase,
+    containUppercase: containUppercase,
+    containNumbers: containNumbers,
+    containSpecial: containSpecial
+  }
+  // console.log(passwordOptions)
+  return passwordOptions;
 }
 
 // Generate the password using the true and false values
@@ -20,5 +45,5 @@ function writePassword(){
 }
 
 document.getElementById('generate').addEventListener('click', () =>{
-writePassword
+writePassword()
 })
